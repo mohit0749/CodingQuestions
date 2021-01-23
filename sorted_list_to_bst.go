b764e7aa99a1ef582ea581bc0230deedcb9042ac
Manish Kushwaha <mohit.ghazipur@gmail.com>
2021-01-23 10:24:57 +0000

    fix question

--- sorted_list_to_bst.go
+++ sorted_list_to_bst.go
@@ -4,32 +4,34 @@ type ListNode struct {
 	data int
 	next *ListNode
 }
-func NewListNode(data int, next *ListNode)*ListNode{
-	return &ListNode{data,next}
+
+func NewListNode(data int, next *ListNode) *ListNode {
+	return &ListNode{data, next}
 }
 
-func printList(head *ListNode){
-	for head!=nil{
-		print(head.data,"->")
+func printList(head *ListNode) {
+	for head != nil {
+		print(head.data, "->")
+		head = head.next
 	}
+	println(" ")
 }
-
 
 type TreeNode struct {
-	data  int
+	data        int
 	left, right *TreeNode
 }
 
-func NewTreeNode(data int,left,right *TreeNode)*TreeNode{
-	return &TreeNode{data,left,right}
+func NewTreeNode(data int, left, right *TreeNode) *TreeNode {
+	return &TreeNode{data, left, right}
 }
 
-func traverseTree(root *TreeNode){
-	if root==nil{
+func traverseTree(root *TreeNode) {
+	if root == nil {
 		return
 	}
+	print(root.data, " ")
 	traverseTree(root.left)
-	print(root.data," ")
 	traverseTree(root.right)
 }
 
@@ -39,27 +41,29 @@ func countNodes(head *ListNode) int {
 	n := 0
 	for head != nil {
 		n++
-		head=head.next
+		head = head.next
 	}
 	return n
 }
 
-func constructBST(head *ListNode, n int)*TreeNode{
-	if n<=0{
+func constructBST(head *ListNode, n int) *TreeNode {
+	if n <= 0 {
 		return nil
 	}
-	leftSTree:=constructBST(head,n/2)
-	root:=NewTreeNode(head.data,leftSTree,nil)
-	*head=*head.next
-	root.right=constructBST(head,n-n/2-1)
+	leftSTree := constructBST(head, n/2)
+	root := NewTreeNode(head.data, leftSTree, nil)
+
+	if head.next != nil {
+		*head = *head.next
+	}
+	root.right = constructBST(head, n-n/2-1)
 	return root
 }
 
-
 func main() {
-	head:=&ListNode{1,&ListNode{2,&ListNode{3,&ListNode{4,&ListNode{5,&ListNode{6,&ListNode{7,nil}}}}}}}
+	head := &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, &ListNode{6, &ListNode{7, nil}}}}}}}
 	printList(head)
-	n:=countNodes(head)
-	root:=constructBST(head,n)
+	n := countNodes(head)
+	root := constructBST(head, n)
 	traverseTree(root)
 }
