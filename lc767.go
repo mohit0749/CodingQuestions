15138b5f6071d579841e756429a94dad0f8d8bb3
Manish Kushwaha <mohit.ghazipur@gmail.com>
2021-03-27 15:39:28 +0530

    new questions

--- lc767.go
+++ lc767.go
@@ -1,23 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"sort"
-	"strings"
 )
 
-func reorganizeString(S string) string {
-	s:=[]byte(S)
-	sort.Slice(s,func(i,j int)bool{
-		if s[i]!=s[j]{return true}
-		return false
-	})
-	var ss strings.Builder
-	for _,c:=range s{
-		ss.WriteRune(rune(c))
-	}
-	return ss.String()
+type pair struct {
+	c   byte
+	cnt int
 }
 
-func main(){
+func reorganizeString(S string) string {
+	mp := make(map[byte]int)
+	ss := make([]byte, 0)
+	countPair := make([]pair, 0)
+	for _, c := range S {
+		mp[byte(c)]++
+	}
+	for k := range mp {
+		if mp[k] > (len(S)+1)/2 {
+			return ""
+		}
+		countPair = append(countPair, pair{c: k, cnt: mp[k]})
+	}
+	sort.Slice(countPair, func(i, j int) bool {
+		if countPair[i].cnt <= countPair[j].cnt {
+			return true
+		}
+		return false
+	})
+	for _, pair := range countPair {
+		tmp := make([]byte, 0)
+		for i := 0; i < pair.cnt; i++ {
+			tmp = append(tmp, pair.c)
+		}
+		ss = append(ss, tmp...)
+	}
+	var ans = make([]byte, len(S))
+	var cnt int = len(S) / 2
+	for i := 0; cnt < len(S); i += 2 {
+		ans[i] = ss[cnt]
+		cnt++
 
+	}
+	cnt = 0
+	for i := 1; cnt < len(S)/2; i += 2 {
+		ans[i] = ss[cnt]
+		cnt++
+	}
+	return string(ans)
+}
+
+func main() {
+	s := "ogccckcwmbmxtsbmozli"
+	//s := "abbabbaaab"
+	println(reorganizeString(s))
 }
